95e65eeaa168016b8bf71f9f679bbcc807ec9a8e
Nighthawk <nighthawkvrc@gmail.com>
2026-04-07 18:04:44 -0500

    init with vrc world pack

---
+++ Packages/org.gravinium.jilwer/Editor/ExampleEditorScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class ExampleEditorScript
+{
+    [MenuItem("Gravinium/Jilwer/Test")]
+    static void Test()
+    {
+        EditorUtility.DisplayDialog("Example Script", "Opened This Dialog", "OK");
+    }
+}
